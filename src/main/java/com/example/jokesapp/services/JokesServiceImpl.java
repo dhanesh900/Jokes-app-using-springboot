463f3f257438cd7cd3cee51344c13380da8e04fc
Dhanesh kumar <dhanesh.bcss19@iba-suk.edu.pk>
2022-04-16 13:07:52 +0500

    added jok service. closes #4

---
+++ src/main/java/com/example/jokesapp/services/JokesServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl() {
+        this.chuckNorrisQuotes=new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
